21647213afd2c74c9e7c077cac9fc4afe9c20f07
KislitsinDA <kislicin1417@gmail.com>
2022-11-03 11:27:03 +0500

    Add new function in main.py

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def napyqt(*args):
+    print(args)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    napyqt(1, 2, 3, 4)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
